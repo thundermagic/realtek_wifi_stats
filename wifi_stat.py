97e56ffac60ad40e84c0e174372e871f1a017c0e
thundermagic <nitin.sidhu23@gmail.com>
2019-08-16 22:33:15 +0100

    correction

--- wifi_stat.py
+++ wifi_stat.py
@@ -50,23 +50,23 @@ if __name__ == '__main__':
         # Parse the output
         freq = iface_stats[iface_stats.find('Frequency'): iface_stats.find('GHz', iface_stats.find('Frequency'))].split(':')[-1]
         freq = 'wifi_frequency[node="{0}",device="{1}"] {2}'.format(sys.argv[2], sys.argv[1], freq)
-        print(freq.replace('[', '{').replace(']', '}')+'\r\n')
+        print(freq.replace('[', '{').replace(']', '}'))
 
         link_quality = iface_stats[iface_stats.find('Link Quality'): iface_stats.find('Signal', iface_stats.find('Link Quality'))].split('=')[-1]
         link_quality = 'wifi_link_quality[node="{0}",device="{1}"] {2}'.format(sys.argv[2], sys.argv[1], link_quality.split('/')[0])
-        print(link_quality.replace('[', '{').replace(']', '}')+'\r\n')
+        print(link_quality.replace('[', '{').replace(']', '}'))
 
         signal_level = iface_stats[iface_stats.find('Signal level'): iface_stats.find('Noise', iface_stats.find('Signal level'))].split('=')[-1]
         signal_level = 'wifi_signal_level[node="{0}",device="{1}"] {2}'.format(sys.argv[2], sys.argv[1], signal_level.split('/')[0])
-        print(signal_level.replace('[', '{').replace(']', '}')+'\r\n')
+        print(signal_level.replace('[', '{').replace(']', '}'))
 
         noise_level = iface_stats[iface_stats.find('Noise level'): iface_stats.find('\n', iface_stats.find('Noise level'))].split('=')[-1]
         noise_level = 'wifi_noise_level[node="{0}",device="{1}"] {2}'.format(sys.argv[2], sys.argv[1], noise_level.split('/')[0])
-        print(noise_level.replace('[', '{').replace(']', '}')+'\r\n')
+        print(noise_level.replace('[', '{').replace(']', '}'))
 
         rssi = rx_signal_stats[rx_signal_stats.find('rssi'): rx_signal_stats.find('\n', rx_signal_stats.find('rssi'))].split(':')[-1]
         rssi = 'wifi_rssi_level[node="{0}",device="{1}"] {2}'.format(sys.argv[2], sys.argv[1], rssi)
-        print(rssi.replace('[', '{').replace(']', '}')+'\r\n')
+        print(rssi.replace('[', '{').replace(']', '}'))
     except Exception as err:
         print('Unexpected error')
         print(traceback.format_exc())
